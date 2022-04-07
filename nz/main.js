e382f00e4deeb1718ac0d955db944867cdebcf4e
Andrea Bruckmeier <andrea.bruckmeier@uibk.ac.at>
2022-04-07 16:38:30 +0200

    Koordinaten für Popup berichtigt

--- nz/main.js
+++ nz/main.js
@@ -5,13 +5,6 @@ let lng = 176.070833;
 let zoom = 11;
 
 let coords = [-38.684444, 176.070833];
-let popup = `
-    <h3>Taupo</h3>
-    <ul>
-    <li>geogr.Länge: ${lng}</li>
-    <li>geogr.Breite: ${lat}</li>
-    </ul>
-    `;
 
 console.log(coords);
 console.log(coords[0]);
@@ -23,10 +16,26 @@ console.log('text');
 console.log('id="map"');
 console.log(`latitude = ${lat}`);
 console.log(ETAPPEN);
+console.log(ETAPPEN[0]);
+console.log(ETAPPEN[0].nr);
+console.log(ETAPPEN[0].github);
+console.log(ETAPPEN[0].titel);
+console.log(ETAPPEN[0].wikipedia);
+console.log(ETAPPEN[0].lat);
+console.log(ETAPPEN[0].lng);
 
 
+let popup = `
+    <h3>Taupo (Stadt)</h3>
+    <ul>
+        <li>geogr. Länge: ${lng}</li>
+        <li>geogr. Breite: ${lat}</li>
+    </ul>
+    `;
 
-let map = L.map('map').setView(lat, lng, zoom);
+
+    
+let map = L.map('map').setView(coords, zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
