71dbd7f996d74851782a82cb877690afa464b60d
Andrea Bruckmeier <andrea.bruckmeier@uibk.ac.at>
2022-04-07 15:44:57 +0200

    lat, lng, zoom Variablen eingeführt

--- nz/main.js
+++ nz/main.js
@@ -1,12 +1,15 @@
 /* Neuseelandreise Skript */
 // einzelner Kommentar
+let lat = -38.684444;
+let lng = 176.070833;
+let zoom = 11;
 
-var map = L.map('map').setView([-38.684444, 176.070833], 11);
+let map = L.map('map').setView([lat, lng], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-38.684444, 176.070833]).addTo(map)
+L.marker([lat, lng]).addTo(map)
     .bindPopup('<h3>Taupo (Stadt)</h3>')
     .openPopup();
