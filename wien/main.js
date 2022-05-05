0a372dc64bca515e69c12bf0162e0495c81b2f8d
Andrea Bruckmeier <andrea.bruckmeier@uibk.ac.at>
2022-05-05 16:26:06 +0200

    Haltestellen Vienna Sightseeing hinzugefügt

--- wien/main.js
+++ wien/main.js
@@ -54,7 +54,7 @@ let stephansdom = {
       
       ).addTo(map);
 
-      //Sehenswürdigkeiten
+//Sehenswürdigkeiten
     async function loadSites(url) {
         let response = await fetch(url);
         let geojson = await response.json();
@@ -68,3 +68,15 @@ let stephansdom = {
     }
     loadSites("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:SEHENSWUERDIGOGD&srsName=EPSG:4326&outputFormat=json")
     
+    //Haltestellen Vienna Sightseeing
+    async function loadStops(url) {
+        let response = await fetch(url);
+        let geojson = await response.json();
+
+        let overlay = L.featureGroup();
+        layerControl.addOverlay(overlay, "Vienna Sightseeing Haltestellen");
+        overlay.addTo(map);
+
+        L.geoJSON(geojson).addTo(overlay);
+    }
+    loadStops("https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:TOURISTIKHTSVSLOGD&srsName=EPSG:4326&outputFormat=json");
