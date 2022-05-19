50dd90d0d3462d0cbb0ce38ec7b5d76b05d40a28
Andrea Bruckmeier <andrea.bruckmeier@uibk.ac.at>
2022-05-19 17:20:37 +0200

    Farbe für TempWerte und Schwellen hinzugefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -65,7 +65,7 @@ let getColor = function(value, ramp){
         }
     }
 };
-console.log(getColor(-40, COLORS.temperature));
+//console.log(getColor(-40, COLORS.temperature));
 
 // Stationen
 let drawStations = function(geojson) {
@@ -103,6 +103,10 @@ let drawTemperature = function(geojson) {
                 <strong>${geoJsonPoint.properties.name}</strong><br>
                 (${geoJsonPoint.geometry.coordinates[2]} m ü.NN)
                 `;
+                let color = getColor(
+                    geoJsonPoint.properties.LT,
+                    COLORS.temperature
+                );
                 return L.marker(latlng, {
                 icon: L.divIcon({
                     className: "aws-div-icon",
