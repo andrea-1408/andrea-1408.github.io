a131bb12f27a8f81079b5e5fce4dbe7072eddded
Andrea Bruckmeier <andrea.bruckmeier@uibk.ac.at>
2022-05-23 09:47:58 +0200

    Relative Feuchtigkeit zu Karte hinzugefügt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -185,7 +185,7 @@ let drawWind = function (geojson) {
 let drawHumidity = function (geojson) {
     L.geoJSON(geojson, {
         filter: function(geoJsonPoint) {
-            if (geoJsonPoint.properties.RL > 0 && geoJsonPoint.properties.RL < 100) {
+            if (geoJsonPoint.properties.RH >= 0 && geoJsonPoint.properties.RH <= 100) {
                 return true
             };
         },
